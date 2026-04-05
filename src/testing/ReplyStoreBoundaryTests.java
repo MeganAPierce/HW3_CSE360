4a05912e568820aed8d25bd317db72bf0bf0cb17
MeganPierce <meganpierce08@gmail.com>
2026-04-05 16:32:25 -0500

    Planned files

---
+++ src/testing/ReplyStoreBoundaryTests.java
@@ -0,0 +1,15 @@
+/*Testing for PostStore only
+* Good tests should:
+* valid login succeeds
+* invalid password fails
+* nonexistent user fails
+* blank username/password fails
+* sensitive behavior is not leaked through messaging
+* SUPPORTS CWE-306 & Cryptographic Failures
+*/
+
+package testing;
+
+public class ReplyStoreBoundaryTests {
+    
+}
