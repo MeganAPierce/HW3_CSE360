e9bcc361e0c3c8e27a4aff793d6ed8a6a0cbd045
MeganPierce <meganpierce08@gmail.com>
2026-04-06 16:57:16 -0500

    correcting module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,22 @@
 module name {
-    
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.sql;     // <-- THIS is what you are missing
+    requires java.desktop; // sometimes needed for UI-related stuff
+
+    opens applicationMain to javafx.fxml;
+    opens guiUserLogin to javafx.fxml;
+    opens guiNewAccount to javafx.fxml;
+    opens guiUserUpdate to javafx.fxml;
+    opens guiViewPosts to javafx.fxml;
+    opens guiRole1 to javafx.fxml;
+    opens guiRole2 to javafx.fxml;
+    opens guiAdminHome to javafx.fxml;
+    opens guiFirstAdmin to javafx.fxml;
+    opens guiAddRemoveRoles to javafx.fxml;
+    opens guiMultipleRoleDispatch to javafx.fxml;
+    opens guiTools to javafx.fxml;
+
+    exports applicationMain;
 }
