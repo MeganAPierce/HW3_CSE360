3261bb953959a8ade12afd1f4a852efa3e67849a
MeganPierce <meganpierce08@gmail.com>
2026-04-06 18:28:00 -0500

    javadoc comments

--- src/entityClasses/PrivateReply.java
+++ src/entityClasses/PrivateReply.java
@@ -11,21 +11,39 @@ public class PrivateReply {
     private String replyId;
     private String authorUserName;
     private String parentPostId;
-
+    /**
+     * Constructs a PrivateReply object
+     * 
+     * @param replyId
+     * @param authorUserName
+     * @param parentPostId
+     */
     public PrivateReply(String replyId, String authorUserName, String parentPostId){
         this.replyId = replyId;
         this.authorUserName = authorUserName;
         this.parentPostId = parentPostId;
     }
 
+    /**
+     * 
+     * @return the reply ID
+     */
     public String getReplyId(){
         return replyId;
     }
 
+    /**
+     * 
+     * @return the username of the reply author
+     */
     public String getAuthorUserName(){
         return authorUserName;
     }
 
+    /**
+     * 
+     * @return the parent post ID
+     */
     public String getParentPostId(){
         return parentPostId;
     }
--- src/entityClasses/StaffPrivatePost.java
+++ src/entityClasses/StaffPrivatePost.java
@@ -4,27 +4,45 @@ package entityClasses;
  * Title: StaffPrivatePost
  * @author Megan Pierce
  * 
- * Description: A lightweight prototype model representing a private post created by staff for a sepcific recipient
+ * Description: A lightweight prototype model representing a private post created by staff for a specific recipient
  */
 public class StaffPrivatePost {
     private String postId;
     private String authorUserName;
     private String recipientUserName;
 
+    /**
+     * StaffPrivatePost
+     * Construcst a StaffPrivatePost object
+     * 
+     * @param postId unique postID - identifier for the post
+     * @param authorUserName the staff user who created the post
+     * @param recipientUserName the intended recipient of the post
+     */
     public StaffPrivatePost(String postId, String authorUserName, String recipientUserName){
         this.postId = postId;
         this.authorUserName = authorUserName;
         this.recipientUserName = recipientUserName;
     }
 
+    /**
+     * 
+     * @return the post ID
+     */
     public String getPostId(){
         return postId;
     }
-
+    /**
+     * 
+     * @return the username of the staff author
+     */
     public String getAuthorUserName(){
         return authorUserName;
     }
-
+    /**
+     * 
+     * @return the username of the intended recipient
+     */
     public String getRecipientUserName(){
         return recipientUserName;
     }
